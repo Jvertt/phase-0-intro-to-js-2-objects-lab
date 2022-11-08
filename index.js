d98cde30fd1542cde7e01a760ea4218058217dbe
Justin Verthein <Justinmackay88@hotmail.com>
2022-11-08 11:25:40 -1000

    commit tree c0cd8da1b8584330a41d543d0b3a8e62bb6b8ff6

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "sam",
+    streetAddress: "11 broadway",
+
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+  
+    newObj[key] = value;
+  
+    return newObj;
+  }
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; 
+  
+    return obj;
+  }
+
+  function deleteFromEmployeeByKey(obj, key, value) {
+    const newObj = { ...obj };
+  
+    newObj[key] = value;
+  
+    return newObj;
+  }
+  
+  const newEmployee = deleteFromEmployeeByKey(
+    employee,
+    "name",
+    "same"
+  );
+
+  function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    obj[key] = value; 
+  
+    return obj;
+  }
